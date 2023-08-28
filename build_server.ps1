2646e8e808b595df2c8f0885ec937ecccac9ba5d
Sky Z <qe201020335@sina.com>
2023-08-28 14:59:28 -0400

    show output

--- build_server.ps1
+++ build_server.ps1
@@ -18,3 +18,4 @@ ls ./build
 Compress-Archive -Path ./build/* -DestinationPath "../SPT-Aki-Server-$Head.zip"
 echo "Built file: SPT-Aki-Server-$Head.zip"
 echo "ZIP_NAME=SPT-Aki-Server-$Head.zip" >> "$GITHUB_OUTPUT"
+cat "$GITHUB_OUTPUT"
