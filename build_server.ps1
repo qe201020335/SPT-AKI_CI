4af05df69cf678adaff8793d9158f9543aaa7626
Sky Z <qe201020335@sina.com>
2023-08-28 14:48:37 -0400

    use build script

---
+++ build_server.ps1
@@ -0,0 +1,20 @@
+echo "clone repo"
+git clone https://dev.sp-tarkov.com/SPT-AKI/Server.git ./Server
+
+echo "lfs"
+cd ./Server
+git lfs fetch
+git lfs pull
+
+$Head = git rev-parse --short HEAD
+
+echo "build"
+cd ./project
+npm install
+npm run build:debug
+
+ls ./build
+
+Compress-Archive -Path ./build/* -DestinationPath "../SPT-Aki-Server-$Head.zip"
+echo "Built file: SPT-Aki-Server-$Head.zip"
+echo "ZIP_NAME=SPT-Aki-Server-$Head.zip" >> "$GITHUB_OUTPUT"
