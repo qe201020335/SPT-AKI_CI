5142e3b129d77034897a8eb4ffdd56bc91e5ba3d
Sky Z <qe201020335@sina.com>
2023-08-28 16:46:17 -0400

    support branches

--- build_server.ps1
+++ build_server.ps1
@@ -1,20 +1,63 @@
-echo "clone repo"
-git clone https://dev.sp-tarkov.com/SPT-AKI/Server.git ./Server
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $Overwrite,
 
-echo "lfs"
-cd ./Server
+    [Parameter(Mandatory=$false)]
+    [string] $Branch
+)
+
+$SERVER_DIR = "./Server"
+if (Test-Path -Path $SERVER_DIR) {
+    if ($Overwrite -or (Read-Host "$SERVER_DIR exists, delete? [y/n]") -eq 'y') {
+        Write-Output "$SERVER_DIR exists, removing"
+        Remove-Item -Recurse -Force $SERVER_DIR
+    }
+    else
+    {
+        Exit 1
+    }
+}
+
+Write-Output "clone repo"
+if ( $Branch.Length -gt 0 )
+{
+    Write-Output "Cloning branch $Branch"
+    git clone -b $Branch https://dev.sp-tarkov.com/SPT-AKI/Server.git $SERVER_DIR
+} 
+else
+{
+    Write-Output "Branch not given, using default branch"
+    git clone https://dev.sp-tarkov.com/SPT-AKI/Server.git $SERVER_DIR
+}
+
+if($? -eq $false)
+{
+    Write-Output "Clone Failed."
+    Exit 1
+}
+
+
+Set-Location $SERVER_DIR
+
+$Head = git rev-parse --short HEAD
+$Branch = git rev-parse --abbrev-ref HEAD
+
+Write-Output "Current HEAD is at $Head in $Branch"
+
+Write-Output "lfs"
 git lfs fetch
 git lfs pull
 
-$Head = git rev-parse --short HEAD
-
-echo "build"
-cd ./project
+Write-Output "build"
+Set-Location ./project
 npm install
 npm run build:debug
 
-ls ./build
+Get-ChildItem ./build
+$AkiMeta = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
+Write-Output $akiMeta
+$ZipName = "Aki-Server-debug-{0}-{1}-Tarkov{2}.zip" -f $akimeta.akiVersion, $Head, $akimeta.compatibleTarkovVersion
 
-Compress-Archive -Path ./build/* -DestinationPath "../SPT-Aki-Server-$Head.zip"
-echo "Built file: SPT-Aki-Server-$Head.zip"
-echo "ZIP_NAME=SPT-Aki-Server-$Head.zip" >> "$env:GITHUB_OUTPUT"
+Compress-Archive -Path ./build/* -DestinationPath "../$ZipName"
+Write-Output "Built file: $ZipName"
+Write-Output "ZIP_NAME=$ZipName" >> "$env:GITHUB_OUTPUT"
