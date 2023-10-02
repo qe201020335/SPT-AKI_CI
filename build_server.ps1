e789d6972235210f2564d1fa6d92f1bdfddd8451
Sky Z <qe201020335@sina.com>
2023-10-02 01:51:36 -0400

    build server with timeout

--- build_server.ps1
+++ build_server.ps1
@@ -8,6 +8,7 @@ Param(
 
 $ErrorActionPreference = "Stop"
 $SERVER_DIR = "./Server"
+$GULP_TIMEOUT = 60 # 60 sec is planty for compiling the server
 
 if (Test-Path -Path $SERVER_DIR) {
     if ($Overwrite -or (Read-Host "$SERVER_DIR exists, delete? [y/n]") -eq 'y') {
@@ -43,7 +44,41 @@ git lfs pull
 Write-Output "build"
 Set-Location ./project
 npm install
-npm run build:debug
+
+
+Write-Output ("building the server with timeout {0} sec" -f $GULP_TIMEOUT)
+# the gulp task may never return because a file watcher is not being triggered
+$code = {
+    npm run build:debug *>&1
+
+    if ($LASTEXITCODE -ne 0) {
+        throw ("Build failed. Exit code {0}" -f $LASTEXITCODE)
+    }
+}
+$j = Start-Job -ScriptBlock $code
+Wait-Job $j -Timeout $GULP_TIMEOUT
+Write-Output "Job Output: "
+Receive-Job $j
+Write-Output "Forcefully removing the job"
+Remove-Job -force $j
+Write-Output ("Job state: {0}" -f $j.State)
+
+if ($j.State -ne "Completed") {
+    # it did not return, we need to manually re-do the unfinished file watch job
+    Write-Output "Gulp was stuck!"
+    if (!(Test-Path -Path "./build/Aki_Data/Server/configs/core.json")) {
+        Write-Output "Something went wrong, the core json doesn't exist"
+        Exit 1
+    }
+    $CoreJson = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
+    if (!$CoreJson.ContainsKey("commit")) {
+        $CoreJson.commit = git rev-parse HEAD
+    }
+} 
+else {
+    Write-Output "Gulp exited!"
+}
+
 
 if ($IsLinux -eq $true) {
     $Os = "linux"
@@ -51,13 +86,13 @@ if ($IsLinux -eq $true) {
 else {
     $Os = "win"
 }
-Write-Output $Os
+Write-Output "Current OS: " $Os
 
 Get-ChildItem ./build
 $AkiMeta = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
 Write-Output $akiMeta
 $ZipName = "Aki-Server-{0}-debug-{1}-{2}-Tarkov{3}.zip" -f $Os, $akimeta.akiVersion, $Head, $akimeta.compatibleTarkovVersion
 
-Compress-Archive -Path ./build/* -DestinationPath "../$ZipName"
+Compress-Archive -Path ./build/* -DestinationPath "../$ZipName" -Force
 Write-Output "Built file: $ZipName"
 Write-Output "ZIP_NAME=$ZipName" >> "$env:GITHUB_OUTPUT"
