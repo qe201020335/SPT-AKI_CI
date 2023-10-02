9aecd2cfce4b97338d4d667fe325fe84661f861f
Sky Z <qe201020335@sina.com>
2023-10-01 20:53:56 -0400

    build linux server

--- build_server.ps1
+++ build_server.ps1
@@ -1,8 +1,8 @@
 Param(
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [Switch] $Overwrite,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [string] $Branch
 )
 
@@ -14,20 +14,17 @@ if (Test-Path -Path $SERVER_DIR) {
         Write-Output "$SERVER_DIR exists, removing"
         Remove-Item -Recurse -Force $SERVER_DIR
     }
-    else
-    {
+    else {
         Exit 1
     }
 }
 
 Write-Output "clone repo"
-if ( $Branch.Length -gt 0 )
-{
+if ( $Branch.Length -gt 0 ) {
     Write-Output "Cloning branch $Branch"
     git clone -b $Branch https://dev.sp-tarkov.com/SPT-AKI/Server.git $SERVER_DIR
 } 
-else
-{
+else {
     Write-Output "Branch not given, using default branch"
     git clone https://dev.sp-tarkov.com/SPT-AKI/Server.git $SERVER_DIR
 }
@@ -48,10 +45,18 @@ Set-Location ./project
 npm install
 npm run build:debug
 
+if ($IsLinux -eq $true) {
+    $Os = "linux"
+}
+else {
+    $Os = "win"
+}
+Write-Output $Os
+
 Get-ChildItem ./build
 $AkiMeta = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
 Write-Output $akiMeta
-$ZipName = "Aki-Server-debug-{0}-{1}-Tarkov{2}.zip" -f $akimeta.akiVersion, $Head, $akimeta.compatibleTarkovVersion
+$ZipName = "Aki-Server-{0}-debug-{1}-{2}-Tarkov{3}.zip" -f $Os, $akimeta.akiVersion, $Head, $akimeta.compatibleTarkovVersion
 
 Compress-Archive -Path ./build/* -DestinationPath "../$ZipName"
 Write-Output "Built file: $ZipName"
