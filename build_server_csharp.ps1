d93ad41a04351134503ee75f6672f2ef4a472bf2
qe201020335 <qe201020335@sina.com>
2025-11-14 22:01:41 -0500

    Remove checks.dat workaroud code

--- build_server_csharp.ps1
+++ build_server_csharp.ps1
@@ -105,7 +105,7 @@ else {
 
 $BuildTime = Get-Date -Format yyyyMMdd
 
-# There is a bug with the assets csproj build target causing the checks.dat not being copied on the first build
+# There was a bug with the assets csproj build target causing the checks.dat not being copied on the first build
 # dotnet build --property WarningLevel=0 ./Libraries/SPTarkov.Server.Assets/SPTarkov.Server.Assets.csproj -f net9.0 -c $Configuration
 Write-Output "dotnet publish --property WarningLevel=0 ./SPTarkov.Server/SPTarkov.Server.csproj -o ./Build -c $Configuration -r $Runtime --self-contained false -p $SFFlag -p:IncludeNativeLibrariesForSelfExtract=true -p:SptBuildType=$SptBuildType -p:SptVersion=$SPTVersion -p:SptBuildTime=$BuildTime -p:SptCommit=$Head -p:IsPublish=true"
 dotnet publish --property WarningLevel=0 ./SPTarkov.Server/SPTarkov.Server.csproj -o ./Build -c $Configuration -r $Runtime --self-contained false -p $SFFlag -p:IncludeNativeLibrariesForSelfExtract=true -p:SptBuildType=$SptBuildType -p:SptVersion=$SPTVersion -p:SptBuildTime=$BuildTime -p:SptCommit=$Head -p:IsPublish=true
